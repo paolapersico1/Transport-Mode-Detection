c8f734321525c1217c9ffd8e5e03d373a2147d64
Paola <paolapersico95@gmail.com>
2021-04-02 17:28:26 +0200

    utility files added

---
+++ TMD/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import seaborn as sb
+import matplotlib as mpl
+import sklearn
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
