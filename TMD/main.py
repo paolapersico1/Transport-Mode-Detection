8bde2243dded346a9b5045082de8bdd708c659ce
Alessandro Giacche <alejack9@users.noreply.github.com>
2021-04-17 13:22:12 +0200

    Starting

--- TMD/main.py
+++ TMD/main.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import numpy as np
-import seaborn as sb
-import matplotlib.pyplot as plt
-import torch
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
-from sklearn.impute import SimpleImputer
+from data_layer import loadData
+from visualization_priori import plot_y
+# import seaborn as sb
+# import matplotlib.pyplot as plt
+# import torch
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import confusion_matrix
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn import metrics
+# from sklearn.impute import SimpleImputer
 
 if __name__ == '__main__':
-    print("okay")
-
+    X, y, num_classes = loadData()
+    plot_y(y)
+    print(X.shape, y.shape, num_classes)
